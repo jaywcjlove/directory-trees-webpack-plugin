123ee6a29d2eb20e49fc6fd74375212eae75d766
jaywcjlove <398188662@qq.com>
2018-01-05 02:56:44 +0800

    Optimize code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const FS = require('fs');
 const PATH = require('path');
 const DirectoryTree = require('directory-tree-md');
-const mkdirp = require('mkdirp');
+const write = require('write');
 
 /**
  * Generate a json tree representing a directory
@@ -12,15 +12,10 @@ const mkdirp = require('mkdirp');
  */
 module.exports = class DirectoryTreePlugin {
     constructor(options) {
-        let { dir, path, enhance, watch } = options;
+        let { dir, path, enhance, watch, ...otherProps } = options;
         this._options = { dir, path, enhance, watch };
-
-        delete options.dir;
-        delete options.path;
-        delete options.enhance;
-        delete options.watch;
-        this._treeOptions = options;
-        this._watchFiles = [];
+        this._treeOptions = { ...otherProps };
+        this.watchFiles = [];
     }
     saveFileToDir(filePathItem, callback) {
         let { dir, filename, sep = "dir" } = this._options.watch;
@@ -34,36 +29,45 @@ module.exports = class DirectoryTreePlugin {
         if (!FS.existsSync(filePathItem)) {
             return callback();
         }
-        let contentOld = '';
         let content = FS.readFileSync(filePathItem);
 
+        let contentOld = '';
         if (FS.existsSync(writePath)) {
             contentOld = FS.readFileSync(writePath);
         }
-        if (!content || content.toString() === contentOld.toString()) return callback();
-        let oldFile = FS.existsSync(writePath);
-
-        mkdirp(PATH.dirname(writePath), (err) => {
-            if (err) {
-                compilation.errors.push(err);
-                callback();
-            }
-            FS.writeFile(writePath, content, (err) => {
-                if (err) {
-                    this.emitError('\r\nWrite to directory failed: ' + err);
-                    return callback(err);
-                }
-                callback();
-            })
-        })
+        if (contentOld && content.toString() === contentOld.toString()) return callback();
+        write.sync(writePath, content);
+        callback();
     }
+    writeFileSync() {
+        const { dir, path, enhance, watch } = this._options;
+        let tree = null;
+        if (Array.isArray(dir)) {
+            tree = dir.map((path) => DirectoryTree(path, this._treeOptions));
+        } else {
+            tree = DirectoryTree(dir, this._treeOptions);
+        }
+        const modified = enhance ? this._restructure(tree) : tree;
+        const watchFiles = modified;
+        const treeContent = JSON.stringify(modified);
 
-    apply(compiler) {
-        compiler.plugin('compile', this._buildTree.bind(this))
-        compiler.plugin('emit', (compilation, callback) => {
-            let { watch, dir } = this._options;
-            if (watch) {
-                let fileDependencies = this._getAllWatchPath(this._watchFiles);
+        let contentOld = null;
+        if (FS.existsSync(path)) {
+            contentOld = FS.readFileSync(path);
+        }
+        if (!contentOld || treeContent.toString() !== contentOld.toString()) {
+            write.sync(path, treeContent);
+        }
+    }
+    watchMarkdownFile(compilation, callback) {
+        const { dir, path, watch } = this._options;
+        if (watch) {
+            let content = [];
+            if (FS.existsSync(path)) {
+                content = FS.readFileSync(path);
+                if (content) content = JSON.parse(content.toString());
+                let fileDependencies = this._getAllWatchPath(content);
+                // console.log('fileDependencies:', compilation.fileDependencies)
                 fileDependencies.forEach((filePathItem) => {
                     if (compilation.fileDependencies.some(file => file.indexOf(filePathItem) === -1)) {
                         // ...tell webpack to watch file recursively until they appear.
@@ -73,9 +77,17 @@ module.exports = class DirectoryTreePlugin {
                         }
                     }
                 })
-            } else {
-                callback();
             }
+        }
+    }
+    apply(compiler) {
+        compiler.plugin('emit', (compilation, done) => {
+            this.writeFileSync();
+            this.watchMarkdownFile(compilation, done)
+            done()
+        })
+        compiler.plugin('after-resolvers', (compilation, callback) => {
+            this.writeFileSync();
         })
     }
     _getAllWatchPath(arr, pathArr = []) {
@@ -88,40 +100,6 @@ module.exports = class DirectoryTreePlugin {
             }
         })
         return pathArr;
-    }
-    /**
-     * Construct the tree and write out a JSON file
-     *
-     */
-    _buildTree(compilation) {
-        let { dir, path, enhance, watch } = this._options;
-        let tree = null;
-        if (Array.isArray(dir)) {
-            tree = dir.map((path) => {
-                return DirectoryTree(path, this._treeOptions)
-            })
-        } else {
-            tree = DirectoryTree(dir, this._treeOptions)
-        }
-
-        let modified = enhance ? this._restructure(tree) : tree;
-        this._watchFiles = modified;
-        let json = JSON.stringify(modified);
-        let current = FS.existsSync(path) ? FS.readFileSync(path, { encoding: 'utf8' }) : '';
-        if (json !== current) {
-            mkdirp(PATH.dirname(path), (err) => {
-                if (err) {
-                    compilation.errors.push(err);
-                    return callback();
-                }
-                FS.writeFile(path, json, error => {
-                    if (error) {
-                        console.error('\r\n\r\nFailure building directory tree: ', error, '\r\n\r\n')
-                    }
-                })
-            })
-        }
-
     }
     /**
      * Enhance the given `item` and recursively enhance children
